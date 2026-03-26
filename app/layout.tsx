d24a85ba282b3bb80ade56764fa5b55104e724da
alaaeallam <alaaeallam@gmail.com>
2026-03-27 01:20:32 +0200

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import "./styles/globals.scss";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-  title: "Silhouett Sunglasses & Eyewear Store",
-  description: "Shop the latest sunglasses and eyewear at Silhouett.",
+  title: "Berwaz By Rania Maged",
+  description: "Wall art paintings and prints, painting on clothes, bags and shoes, piñata for parties, wedding caricature and much more by Rania Maged, adds a decorative touch to your house making it elegant and stylish. Please, message us for orders or inquiries.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
